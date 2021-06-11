55cba91795f950cf849cdea1b212c1db482fae26
kamballu <kamballu@ualberta.ca>
2021-06-10 18:13:06 -0600

    Update double quotes

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-with open("requirements.txt") as f:
+with open('requirements.txt') as f:
     reqs = f.read()
 
 setup(
-    name="digital-pathology-annotation",
-    version="0.1",
+    name='digital-pathology-annotation',
+    version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=reqs.strip().split("\n"),
-    author="AltalML",
-    description="package_description",
+    install_requires=reqs.strip().split('\n'),
+    author='AltalML',
+    description='package_description',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/altaml/digital-pathology-annotation",
-    classifiers=["Programming Language :: Python :: 3.6"],
-    test_suite="pytest",
+    long_description_content_type='text/markdown',
+    url='https://github.com/altaml/digital-pathology-annotation',
+    classifiers=['Programming Language :: Python :: 3.6'],
+    test_suite='pytest',
 )
