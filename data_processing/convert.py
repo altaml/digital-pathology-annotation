cf715d53b2dd6b88940e7d7d3cc886b322b384a9
kamballu <kamballu@ualberta.ca>
2021-06-10 18:10:37 -0600

    Adjust main script for new svs reader

--- data_processing/convert.py
+++ data_processing/convert.py
@@ -8,31 +8,48 @@ from svs_utils import read_svs
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-parser = argparse.ArgumentParser(description="Convert svs to specifed format")
-parser.add_argument("--path", metavar="src", type=str, help="the path of svs image folder")
-parser.add_argument("--format", metavar="to", type=str, help="format to convert to")
+parser = argparse.ArgumentParser(description='Convert svs to specifed format')
 parser.add_argument(
-    "--output_dir", metavar="dir", type=str, default="converted", help="destination folder"
+    '--path', metavar='src', type=str, help='the path of svs image folder'
+)
+parser.add_argument('--format', metavar='to', type=str, help='format to convert to')
+parser.add_argument('--extract', dest='feature', action='store_true')
+parser.add_argument(
+    '--output_dir',
+    metavar='dir',
+    type=str,
+    default='converted',
+    help='destination folder',
 )
 args = parser.parse_args()
 
 input_path = args.path
 output_format = args.format
 output_path = Path(args.output_dir)
-FORMAT_LIST = [".jpg", ".png", ".tiff"]
+thumbnail = not args.feature
+FORMAT_LIST = ['.jpg', '.png', '.tiff']
 
 if output_format not in FORMAT_LIST:
-    err_msg = f"Invalid format {output_format}, select from {FORMAT_LIST}"
+    err_msg = f'Invalid format {output_format}, select from {FORMAT_LIST}'
     logging.error(err_msg)
     raise IOError(err_msg)
 
 if not Path(input_path).is_dir():
-    logging.error("The path specified does not exist")
+    logging.error('The path specified does not exist')
     sys.exit()
 
 if not Path(output_path).is_dir():
+    logging.info(f'Created {output_path}')
     output_path.mkdir(parents=True, exist_ok=True)
 
-for svs_file in Path(input_path).glob("*.svs"):
-    img = read_svs(svs_file)
-    cv2.imwrite(str(Path(output_path) / (str(svs_file.stem) + str(output_format))), img)
+
+for svs_file in Path(input_path).glob('*.svs'):
+    img_list, thumbnail = read_svs(svs_file, thumbnail_only=thumbnail)
+    # Save thumbnail
+    thumbnail = _run_histogram_equalization(thumbnail)
+
+    output_file = str(
+        Path(output_path) / (svs_file.stem + '_thumbnail' + str(output_format))
+    )
+    cv2.imwrite(output_file, thumbnail)
+    logging.info(f'Thumbnail saved at {output_file}.')
